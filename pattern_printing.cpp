a926edbe36b60c4e9abdab84aa57f186c95cbaa9
ZaidP101 <zpatel044@gmail.com>
2024-08-23 07:39:30 +0530

    p_p completed

--- pattern_printing.cpp
+++ pattern_printing.cpp
@@ -1,5 +1,126 @@
 #include<iostream>
 using namespace std;
 int main(){
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=n; j++){
+    //         cout<<"X";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    /////////////////////////////
+
+    // int n;
+    // cout<<"Number : ";
+    // cin>>n;
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<=n; j++){
+    //         cout<<(char)(j+64)<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    /////////////////////
+
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;    
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=n; j++){
+    //         cout<<char(i + 64)<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+    ///////////////////////
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<"X";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    //////////////////////////
+
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<i;
+    //     }
+    //     cout<<"\n";
+    // }
+
+
+    ////////////////////////
+
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=n-i+1; j++){
+    //         cout<<j;
+    //     }
+    //     cout<<"\n";
+    // }
+
+    // ///////////////////////////////////////  (j<=i) = traingle  ////////////////////////////////////////
+    // ///////////////////////////////////////  (j<=n) = square  ////////////////////////////////////////
+    // ///////////////////////////////////////  (j<=n-i+1) = opp. traingle  ///////////////////////////////
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<2*j-1<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    ////////////////////////////////////////
+
+    // floyed triangle
+
+    // int m, n;
+    // cout<<"Number of ROWS :  ";
+    // cin>>m;
+    // cout<<"Number of COLOUMNS :  ";
+    // cin>>n;
+    // int a =1;
+    // for(int i=1; i<=m; i++){
+    //     for(int j=1; j<=i; j++){
+    //         cout<<a<<" ";
+    //         a++;
+    //     }
+    //     cout<<"\n";
+    // }
+
+    ///////////////////////////////////
+
     
 }
