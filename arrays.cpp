ff7f7999cbf0ee8f30e6fd1d9af96008eb6b135c
ZaidP101 <zpatel044@gmail.com>
2024-08-27 22:30:55 +0530

    strings

--- arrays.cpp
+++ arrays.cpp
@@ -1,5 +1,54 @@
 #include<iostream>
 using namespace std;
 int main(){
-    
+    // int arr[] = {1,2,3,4,5,6,7,8,9};
+    // int sum = 0;
+    // for(int i=0; i<=8; i++){
+    //     cout<<arr[i]<<" ";
+    //     sum += arr[i];
+    // }
+    // cout<<"\n"<<"The sum of the array is : "<<sum<<"\n";
+
+    ///////////////////////////////////////////
+
+    // int arr[] = {235,5,3,56,767,88,654,55,556,67,453, 1000000};
+    // int max = arr[0];
+    // int n = sizeof(arr)/4;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]>max){
+    //         max = arr[i];
+    //     }
+    // }
+    //     cout<<max<<"\n";  
+
+    //////////////////////////////////////////////////////////
+
+    // int arr[5] = {1,2,3,4,5};
+    // int n = sizeof(arr)/4;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]%2==0){
+    //         arr[i] = arr[i] + 10;
+    //     }
+    //     else{
+    //         arr[i] *= 2;
+    //     }
+    //     cout<<arr[i]<<" ";
+    // }
+    // cout<<"\n";
+
+    /////////////////////////////////////// large - second large  ////////////////////////
+
+    // int arr[5] = {1,2,3,400,500};
+    // int n = sizeof(arr)/4;
+    // int mx = INT8_MIN;
+    // for(int i=0; i<n; i++){
+    //     mx = max(arr[i], mx);
+    // }
+    // int smx = INT8_MIN;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]!=mx) smx = max(arr[i], smx);
+    // }
+    // cout<<mx-smx<<"\n";
+
+    ////////////////////////////////    
 }
---
+++ strings.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // string s = "zaid";
+    // cout<<s<<"\n";
+    // cout<<"The lenght of the string is : "<<s.length()<<"\n";
+    // s.push_back(' ');
+    // cout<<s<<"\n";
+    // s.push_back('a');
+    // cout<<s<<"\n";
+    // s.push_back('b');
+    // cout<<s<<"\n";
+    // s.pop_back();
+    // cout<<s<<"\n";
+    // s.append(" new string add through APPEND");
+    // cout<<s<<"\n";
+    // cout<<"The lenght of the string is : "<<s.length()<<"\n";
+    // s = s + ". Can also append through + operator";
+    // cout<<s<<"\n";
+
+
+    //////////////////////////////
+
+    ////////////int to string (to_string)
+    // int x = 64647373;
+    // string s = to_string(x);
+    // cout<<s<<"\n";
+    // cout<<s.length()<<"\n";
+
+    /////////////////////string to int/////////
+
+    // string s = "zaid";
+    // int x = stoi(s);
+    // cout<<x<<"\n";
+}
