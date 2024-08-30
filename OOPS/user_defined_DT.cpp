25dd6c5642bf0d42cea1250aec8199b472eeec81
ZaidP101 <zpatel044@gmail.com>
2024-08-30 22:17:27 +0530

    OOPS > user define data types

---
+++ OOPS/user_defined_DT.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+// class Students{
+// public:
+//     string name;
+//     int roll;
+//     float gpa;
+// };
+// void print(Students s){
+//     cout<<"Name : "<<s.name<<"  Roll - "<<s.roll<<"  GPA = "<<s.gpa<<"\n";
+// }
+// int main(){
+//     cout<<"Enter the Name, Roll No. & GPA of the Student"<<"\n";
+//     Students s1;
+//     cin>>s1.name;
+//     cin>>s1.roll;
+//     cin>>s1.gpa;
+
+
+//     cout<<"Enter the Name, Roll No. & GPA of the next Student"<<"\n";
+//     Students s2;
+//     cin>>s2.name;
+//     cin>>s2.roll;
+//     cin>>s2.gpa;
+
+//     /////////////////////////  coppy ///////////////////////
+     
+//     Students s4 = s1; // deep copy
+//     Students s5(s2); // Copy constructor
+
+    
+//     // cout<<"Name : "<<s1.name<<"  Roll - "<<s1.roll<<"  GPA = "<<s1.gpa<<"\n";
+//     // cout<<"Name : "<<s2.name<<"  Roll - "<<s2.roll<<"  GPA = "<<s2.gpa<<"\n";
+
+//     print(s1);
+//     print(s2);
+//     print(s4);
+//     print(s5);
+// }
+
+
+
+// ///////////////////////////////////// Constructor  ///////////////////////////////////// //
+
+// class Students{
+// public:
+//     string name;
+//     int roll;
+//     float gpa;
+
+//     //////  Constructor /////////
+//     Students(string s, int r,float g){
+//         name = s;
+//         roll = r;
+//         gpa = g;
+//     }
+// };
+// int main(){
+//     Students s3("Ali",33,8.8);
+
+//     cout<<"Name : "<<s3.name<<"  Roll - "<<s3.roll<<"  GPA = "<<s3.gpa<<"\n";
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////// if you want both ways of initialization then, //////////////////////////
+////////////////////////////////////// set the default parameter > while building a //////////////////////////
+////////////////////////////////////// a constructor in class, make an empty constructor ////////////////////
+////////////////////////////////////// Eg :- students(){} //////////////////////////////////////////////////
+////////////////////////////////////// just adding the above will allow you initialized in same class//////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////// similarly multiple constructors are allowed //////////////////////
+
+
+
